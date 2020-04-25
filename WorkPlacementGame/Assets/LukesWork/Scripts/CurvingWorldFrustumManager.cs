fe7600cc73cd0c1c64ccf65cfe30a6ec532921a6
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-04-25 21:51:36 +0100

    Fixed World Curving
    The world now curves without an object's scale affecting the rate the object curves, and the world also doesn't curve until the game is in play mode, to make editing easier.

---
+++ WorkPlacementGame/Assets/LukesWork/Scripts/CurvingWorldFrustumManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteAlways]
+public class CurvingWorldFrustumManager : MonoBehaviour
+{
+    const string CURVING_FEATURE = "ENABLE_BENDING";
+
+    private void Awake()
+    {
+        if (Application.isPlaying)
+        {
+            Shader.EnableKeyword(CURVING_FEATURE);
+        }
+        else
+        {
+            Shader.DisableKeyword(CURVING_FEATURE);
+        }
+    }
+
+    private void OnEnable()
+    {
+        RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
+        RenderPipelineManager.endCameraRendering += OnEndCameraRendering;
+    }
+
+    private void OnDisable()
+    {
+        RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
+        RenderPipelineManager.endCameraRendering -= OnEndCameraRendering;
+    }
+
+    void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera)
+    {
+        camera.cullingMatrix = Matrix4x4.Ortho(-100, 100, -100, 100, 0.1f, 1000) * camera.worldToCameraMatrix;
+    }
+
+    void OnEndCameraRendering(ScriptableRenderContext context, Camera camera)
+    {
+        camera.ResetCullingMatrix();
+    }
+}
