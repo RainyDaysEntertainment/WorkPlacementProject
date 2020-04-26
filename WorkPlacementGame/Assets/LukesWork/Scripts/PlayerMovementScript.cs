9e276d1f83c540a87a0501740b74bc9006d6b253
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-04-26 22:22:13 +0100

    Player Bounce and Rotation
    I added the effect where the placer bounces when fall from a height. This is a change from the player not being able to fall off of ledges previously done with raycasts, but we are now looking for the ability to take fall damage. The player also rotates towards where you are walking.

--- WorkPlacementGame/Assets/LukesWork/Scripts/PlayerMovementScript.cs
+++ WorkPlacementGame/Assets/LukesWork/Scripts/PlayerMovementScript.cs
@@ -6,9 +6,9 @@ public class PlayerMovementScript : MonoBehaviour
 {
     GameObject player;
     Rigidbody rb;
-    float speed = 8;
+    float speed = 6;
     //public LeanTweenType jumpType;
-    public bool onGround, canMove;
+    public bool onGround;
     Vector3 moveDirection;
 
     void Start()
@@ -24,9 +24,17 @@ public class PlayerMovementScript : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
 
-        if (canMove)
+        rb.MovePosition(transform.position + movement);
+
+        if (onGround)
         {
-            rb.MovePosition(transform.position + movement);
+            rb.AddForce(Physics.gravity * rb.mass * 0.25f);
+            rb.useGravity = false;
+        }
+        else
+        {
+            rb.AddForce(Physics.gravity * rb.mass);
+            rb.useGravity = false;
         }
 
         transform.rotation = Quaternion.LookRotation(movement);
@@ -37,35 +45,38 @@ public class PlayerMovementScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
-            //rb.AddForce(Vector3.up * 1000);
+            rb.AddForce((Vector3.up * 10000) + (transform.TransformDirection(Vector3.forward) * 2000));
             //LeanTween.moveY(gameObject, gameObject.transform.position.y + 3f, 0.3f).setEase(jumpType).setLoopPingPong(1);
             //LeanTween.moveY(gameObject, gameObject.transform.position.y, 0.3f).setDelay(0.3f).setEase(jumpType);
         }
 
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position + (transform.TransformDirection(Vector3.forward) / 2), transform.TransformDirection(Vector3.down)/* + (transform.TransformDirection(Vector3.forward) / 2)*/, out hit, 1.7f))/* &&*/
-            //Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down) + transform.TransformDirection(Vector3.right), out hit, 2.2f) &&
-            //Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down) + transform.TransformDirection(Vector3.left), out hit, 2.2f))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1.1f))
+        //Physics.Raycast(transform.position + (transform.TransformDirection(Vector3.forward) / 2), transform.TransformDirection(Vector3.down) + (transform.TransformDirection(Vector3.forward) / 2), out hit, 1.5f))
+        //Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down) + transform.TransformDirection(Vector3.right), out hit, 2.2f) &&
+        //Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down) + transform.TransformDirection(Vector3.left), out hit, 2.2f))
         {
             onGround = true;
         }
         else
         {
+            //Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit2, Mathf.Infinity);
+            //var distanceToGround = hit2.distance;
+            //transform.position = new Vector3(transform.position.x, distanceToGround/* + transform.GetComponent<Collider>().bounds.extents.y*/, transform.position.z);
+
             onGround = false;
         }
 
         if (onGround)
         {
             //gameObject.transform.position = new Vector3(gameObject.transform.position.x, hit.transform.position.y + gameObject.transform.position.y, gameObject.transform.position.z);
-            canMove = true;
         }
         else
         {
-            canMove = false;
         }
 
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 1.1f, Color.yellow);
         Debug.DrawRay(transform.position + (transform.TransformDirection(Vector3.forward) / 2), (transform.TransformDirection(Vector3.down)/* + transform.TransformDirection(Vector3.forward) / 2*/) * 1.7f, Color.yellow);
     }
 }
