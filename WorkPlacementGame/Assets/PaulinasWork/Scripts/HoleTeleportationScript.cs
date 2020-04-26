da50f0819ac9783c0d5c7a351b07e11dc72aa8c3
Paulina Wozniak <S00176242@mail.itsligo.ie>
2020-04-26 15:33:28 +0100

    created cave blockout and tested level

---
+++ WorkPlacementGame/Assets/PaulinasWork/Scripts/HoleTeleportationScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleTeleportationScript : MonoBehaviour
+{
+    public GameObject newHole;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (GameObject.FindGameObjectWithTag("Player").transform.position == other.transform.position)
+        {
+            GameObject.FindGameObjectWithTag("Player").transform.position = newHole.transform.position;
+        }
+    }
+}
