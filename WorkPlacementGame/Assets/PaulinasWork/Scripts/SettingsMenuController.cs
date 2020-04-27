87f3f9a9727b0004818743f7bc4a6d3b0182506b
Paulina Wozniak <S00176242@mail.itsligo.ie>
2020-04-27 10:19:43 +0100

    Main Menu and Settings menu
    basic main menu completed, settings menu in progress

---
+++ WorkPlacementGame/Assets/PaulinasWork/Scripts/SettingsMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenuController : MonoBehaviour
+{
+
+    public GameObject settingsMenu;
+    public GameObject mainMenu;
+    public Button backBtn;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Back()
+    {
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
