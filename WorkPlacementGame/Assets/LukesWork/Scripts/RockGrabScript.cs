c8e96a77e02dea2810a9ebd30d0f7326ca36520b
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-04-30 07:12:07 +0100

    Optimising clouds and render textures

---
+++ WorkPlacementGame/Assets/LukesWork/Scripts/RockGrabScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockGrabScript : MonoBehaviour
+{
+    public bool grabbing;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && grabbing)
+        {
+            grabbing = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Grab")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+                grabbing = true;
+        }
+    }
+}
