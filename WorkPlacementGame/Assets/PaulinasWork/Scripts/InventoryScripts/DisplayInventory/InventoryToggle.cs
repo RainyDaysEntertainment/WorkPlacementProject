1a8749fdda9fa462c542004f9b62159faa34ccb9
Paulina Wozniak <S00176242@mail.itsligo.ie>
2020-05-08 22:16:05 +0100

    Shaders and Inventory
    Created a basic wall shader with normals, also updated inventory display, still needs a lot more work though

---
+++ WorkPlacementGame/Assets/PaulinasWork/Scripts/InventoryScripts/DisplayInventory/InventoryToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryToggle : MonoBehaviour
+{
+    public Canvas canvas;
+    public bool isOpen = false;
+    void Start()
+    {
+        canvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            canvas.enabled = !canvas.enabled;
+        }
+
+        if (canvas.enabled == true)
+        {
+            isOpen = true;
+        }
+        else
+        {
+            isOpen = false;
+        }
+    }
+}
