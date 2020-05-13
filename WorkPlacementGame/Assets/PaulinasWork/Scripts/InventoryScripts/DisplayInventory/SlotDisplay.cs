52e045ec904a9ec113b1e9918a75d4e7c636cfeb
Paulina Wozniak <S00176242@mail.itsligo.ie>
2020-05-13 13:56:51 +0100

    Tooltip
    Not working fully yet, doesn't display the info right yet

--- WorkPlacementGame/Assets/PaulinasWork/Scripts/InventoryScripts/DisplayInventory/SlotDisplay.cs
+++ WorkPlacementGame/Assets/PaulinasWork/Scripts/InventoryScripts/DisplayInventory/SlotDisplay.cs
@@ -3,24 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SlotDisplay : MonoBehaviour
+public class SlotDisplay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Inventory inventory;
     Dictionary<Slot, GameObject> itemsDisplay = new Dictionary<Slot, GameObject>();
     public int x_space;
     public int y_space;
     public int colmn;
+    private GameObject tooltip;
+    private TextMeshProUGUI tiptext;
 
     public void Start()
     {
+        tooltip = GameObject.Find("toolTip").GetComponent<Image>().gameObject;
+        tiptext = GameObject.Find("tipText").GetComponent<TextMeshProUGUI>();
+        tooltip.SetActive(false);
         Create();
     }
 
     private void Update()
     {
         UpdateDisplay();
+        DisplayInfo();
     }
 
     private void UpdateDisplay()
@@ -60,4 +67,36 @@ public class SlotDisplay : MonoBehaviour
         return new Vector3(x_space *(i % colmn), (-y_space * (i/colmn)), 0f);
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ShowToolTip();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        HideToolTip();
+    }
+
+    public void ShowToolTip()
+    {
+        tooltip.SetActive(true);
+    }
+
+    public void HideToolTip()
+    {
+        tiptext.text = "";
+        tooltip.SetActive(false);
+    }
+
+    public void DisplayInfo()
+    {
+        for (int i = 0; i < inventory.InventoryItems.Count; i++)
+        {
+            if (itemsDisplay.ContainsKey(inventory.InventoryItems[i]))
+            {
+                tiptext.text
+                   = inventory.InventoryItems[i].item.itemName.ToString() + " " + inventory.InventoryItems[i].item.itemDescription.ToString();
+            }
+        }
+    }
 }
