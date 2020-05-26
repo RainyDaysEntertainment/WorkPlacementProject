a1303b109d04531ae425c5f0c02229ed663601ed
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-05-26 09:01:40 +0100

    Added Paulina Branch

--- WorkPlacementGame/Assets/PaulinasWork/Scripts/InventoryScripts/DisplayInventory/SlotDisplay.cs
+++ WorkPlacementGame/Assets/PaulinasWork/Scripts/InventoryScripts/DisplayInventory/SlotDisplay.cs
@@ -13,21 +13,21 @@ public class SlotDisplay : MonoBehaviour//, IPointerEnterHandler, IPointerExitHa
     public int x_space;
     public int y_space;
     public int colmn;
-    private GameObject tooltip;
-    private TextMeshProUGUI tiptext;
+   // private GameObject tooltip;
+   // private TextMeshProUGUI tiptext;
 
     public void Start()
     {
-        tooltip = GameObject.Find("toolTip").GetComponent<Image>().gameObject;
-        tiptext = GameObject.Find("tipText").GetComponent<TextMeshProUGUI>();
-        tooltip.SetActive(false);
+        //tooltip = GameObject.Find("toolTip").GetComponent<Image>().gameObject;
+        //tiptext = GameObject.Find("tipText").GetComponent<TextMeshProUGUI>();
+        //tooltip.SetActive(false);
         Create();
     }
 
     private void Update()
     {
         UpdateDisplay();
-        DisplayInfo();
+
     }
 
     private void UpdateDisplay()
@@ -65,38 +65,5 @@ public class SlotDisplay : MonoBehaviour//, IPointerEnterHandler, IPointerExitHa
     public Vector3 GetPosition(int i)
     {
         return new Vector3(x_space *(i % colmn), (-y_space * (i/colmn)), 0f);
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        ShowToolTip();
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        HideToolTip();
-    }
-
-    public void ShowToolTip()
-    {
-        tooltip.SetActive(true);
-    }
-
-    public void HideToolTip()
-    {
-        tooltip.SetActive(false);
-        tiptext.text = "";
-    }
-
-    public void DisplayInfo()
-    {
-        for (int i = 0; i < inventory.InventoryItems.Count; i++)
-        {
-            if (itemsDisplay.ContainsKey(inventory.InventoryItems[i]))
-            {
-                tiptext.text
-                   = inventory.InventoryItems[i].item.itemName.ToString() + " " + inventory.InventoryItems[i].item.itemDescription.ToString();
-            }
-        }
     }
 }
