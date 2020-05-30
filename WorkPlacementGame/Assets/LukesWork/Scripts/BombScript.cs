28e88387f6e95addb761df378960fcfe2e366dbd
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-05-31 00:29:35 +0100

    Main Menu Effects and Bomb

---
+++ WorkPlacementGame/Assets/LukesWork/Scripts/BombScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    public float knockbackStrength, knockbackRadius;
+    private MeleeScript melee;
+    public LayerMask enemyLayers;
+    public GameObject bomb, obj;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            obj = Instantiate(bomb, gameObject.transform.position - new Vector3(0, 0.5f, 0), Quaternion.identity);
+            Invoke("Explode", 3);
+            Destroy(obj, 3);
+        }
+    }
+
+    void Explode()
+    {
+        Collider[] hitEnemies = Physics.OverlapSphere(transform.position, knockbackRadius, enemyLayers);
+
+        foreach (Collider enemy in hitEnemies)
+        {
+            Rigidbody rb = enemy.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(knockbackStrength, obj.transform.position + new Vector3(0, -1, 0), knockbackRadius, 1.5f, ForceMode.Impulse);
+            }
+        }
+
+        //Destroy(gameObject);
+    }
+}
---
+++ WorkPlacementGame/Assets/LukesWork/Scripts/TitleNameScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleNameScript : MonoBehaviour
+{
+    Color colour;
+    public bool lerping;
+
+    void OnEnable()
+    {
+        Invoke("StartLerp", 1);
+    }
+
+    void Update()
+    {
+        if (lerping)
+        {
+            gameObject.GetComponent<Image>().color = Color.Lerp(
+                gameObject.GetComponent<Image>().color,
+                new Color(1, 1, 1, 1), Time.deltaTime * 0.5f);
+        }
+
+        if (gameObject.GetComponent<Image>().color == new Color(1, 1, 1, 0.99f))
+        {
+            lerping = false;
+        }
+    }
+
+    void StartLerp()
+    {
+        lerping = true;
+        CancelInvoke();
+    }
+}
