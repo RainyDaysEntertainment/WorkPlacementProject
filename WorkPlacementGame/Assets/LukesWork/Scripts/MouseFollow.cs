c677c22f2c786152fc0591a607e51017b9ce8831
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-06-02 03:58:40 +0100

    Leaves and Mouse Trail

---
+++ WorkPlacementGame/Assets/LukesWork/Scripts/MouseFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    public float speed = 5, dis = 5;
+    Camera cam;
+
+    private void Start()
+    {
+        cam = GameObject.Find("Camera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = dis;
+        
+        Vector3 mouseScreenToWorld = cam.ScreenToWorldPoint(mousePos);
+        Vector3 pos = Vector3.Lerp(transform.position, mouseScreenToWorld, 1.0f - Mathf.Exp(-speed * Time.deltaTime));
+
+        transform.position = pos;
+    }
+}
