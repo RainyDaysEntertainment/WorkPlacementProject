4e060eb9bf287d3be243931f48d112c79a6bc81d
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-06-16 04:31:47 +0100

    Paths, Torches, Fire facing camera

---
+++ WorkPlacementGame/Assets/LukesWork/Scripts/FaceCameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCameraScript : MonoBehaviour
+{
+    Camera cam;
+
+    private void Start()
+    {
+        cam = GameObject.Find("Camera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.down);
+    }
+}
