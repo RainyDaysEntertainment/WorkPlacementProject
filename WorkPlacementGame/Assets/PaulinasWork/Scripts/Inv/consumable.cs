e5a5f0d6b069bea5d46337fad6b4e605268cd8f8
Paulina Wozniak <S00176242@mail.itsligo.ie>
2020-06-20 14:06:59 +0100

    new inventory system

---
+++ WorkPlacementGame/Assets/PaulinasWork/Scripts/Inv/consumable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class consumable : MonoBehaviour
+{
+    public string Name;
+    public string Description;
+    public int Health;
+    public Image icon;
+}
---
+++ WorkPlacementGame/Assets/PaulinasWork/Scripts/Inv/consumableSetup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class consumableSetup : MonoBehaviour
+{
+    consumable consumable;
+    int amount;
+    TextMeshProUGUI amtTxt;
+    Texture Icon;
+
+    private void Start()
+    {
+        amtTxt = GameObject.Find("amountText").GetComponent<TextMeshProUGUI>();
+        Icon = GameObject.Find("consumableIcon").GetComponent<Texture>();
+        amount = 0;
+    }
+
+    private void Update()
+    {
+        Icon = consumable.icon.mainTexture;
+        amtTxt.text = amount.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<consumable>();
+        if(item)
+        {
+            amount++;
+            Destroy(other.gameObject);
+        }
+    }
+}
