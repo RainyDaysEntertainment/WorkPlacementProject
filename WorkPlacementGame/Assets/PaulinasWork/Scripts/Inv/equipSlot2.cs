977865c8d2e1df33fb3c30b363e251538bf3c129
Paulina Wozniak <S00176242@mail.itsligo.ie>
2020-06-29 15:54:28 +0100

    inventory progress

---
+++ WorkPlacementGame/Assets/PaulinasWork/Scripts/Inv/equipSlot2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class equipSlot2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    GameObject slot;
+    GameObject icon;
+    bool hover;
+    void Start()
+    {
+        slot = GameObject.Find("equip2");
+        icon = GameObject.Find("eq2img");
+    }
+
+    void Update()
+    {
+        if (hover == true)
+        {
+
+        }
+        if (hover == false)
+        {
+
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Enter");
+        hover = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Exit");
+        hover = false;
+    }
+}
