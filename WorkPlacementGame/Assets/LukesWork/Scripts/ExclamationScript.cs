3367c873b87dc736e80a52b33adda1ee5b35ec9e
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-08-13 21:47:28 +0100

    Sparkle and Exclaim

---
+++ WorkPlacementGame/Assets/LukesWork/Scripts/ExclamationScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExclamationScript : MonoBehaviour
+{
+    Camera cam;
+    GameObject image;
+
+    void Start()
+    {
+        image = GetComponentInChildren<Image>().gameObject;
+        image.SetActive(false);
+        cam = GameObject.Find("Camera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        gameObject.transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward,
+            cam.transform.rotation * Vector3.up);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+            image.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+            image.SetActive(false);
+    }
+}
